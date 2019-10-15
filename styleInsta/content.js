fb71be707bae7749adf329cab3ab8936580a4747
Kaiofprates <kaioprates@hotmail.com>
2019-10-15 00:19:44 -0300

    Melhorando estilização do instagram

--- styleInsta/content.js
+++ styleInsta/content.js
@@ -2,4 +2,30 @@ var barra = document.getElementsByClassName('SCxLW  o64aR');
 barra[0].style["backgroundColor"] = "#27334c";
 //var menu = document.getElementsByClassName('MWDvN'); 
 //menu[0].style["backgroundColor"] = "#a847ff";
+var a = document.getElementsByClassName('COOzN');
+a[0].style['padding'] = '10px';
+a[0].style['backgroundColor'] = "#7B68EE";
+a[0].style["borderRadius"] = "30px";
+var b = document.getElementsByClassName("f5Yes oL_O8");
+b[0].style['color'] = "#F5FFFA";
+b[0].style['paddingLeft'] = "50px";
+b[0].style['fontWeight'] = "bold";
+b[0].style['fontSize'] = '15px';
+var t = document.getElementsByClassName('_lz6s');
+t[0].style['backgroundColor'] = "#873CE3";
+var i = document.getElementsByClassName("_8-yf5 ");
+i[0].style["display"] = "none";
+var l = document.getElementsByClassName("s4Iyt");
+l[0].style["marginLeft"] = "150px";
+l[0].style["height"] = "40px"; 
+var o = document.getElementsByClassName('_47KiJ'); 
+o[0].style["marginRight"] = "50px";
+
+
+
+
+
+
+
+
 
